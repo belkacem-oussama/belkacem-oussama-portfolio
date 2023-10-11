73f432881aa5643d949f2fec5bce02bdcc1fb6b0
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-11 08:51:26 +0200

    add loader entrance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
+import { useState, useEffect } from "react";
 import Home from "./pages/Home.jsx";
 import Header from "./layouts/header.jsx";
-
 import "./assets/styles/import.scss";
 import Footer from "./layouts/Footer.jsx";
+import Loader from "./components/Loader.jsx";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Home />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
