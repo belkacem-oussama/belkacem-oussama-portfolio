9c420bbed04a7477acf988b1f0829854f99fc229
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-11 09:59:43 +0200

    update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+
+import Header from "./layouts/Header.jsx";
 import Home from "./pages/Home.jsx";
-import Header from "./layouts/header.jsx";
-import "./assets/styles/import.scss";
 import Footer from "./layouts/Footer.jsx";
 import Loader from "./components/Loader.jsx";
+
+import "./assets/styles/import.scss";
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
