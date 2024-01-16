54b8a9338fba5b8a26abdc76b597c63150f9f5e1
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-01-16 14:26:19 +0100

    display the good one

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,5 +1,26 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import text from '../assets/json/text.json'
 
 export default function Project() {
-    return <div>Project</div>
+    const { id } = useParams()
+    const globalJson = Object.values(text.projects.project)
+
+    const selectedProject = globalJson.find(
+        (project) => project.id === parseInt(id)
+    )
+
+    return (
+        <div className="project-container">
+            <div className="project-title">{selectedProject.title}</div>
+            <div className="project-technologies">
+                {selectedProject.technologies}
+            </div>
+            <div className="project-description">
+                {selectedProject.description}
+            </div>
+            <div className="project-img">
+                <img src={selectedProject.image} alt={selectedProject.title} />
+            </div>
+        </div>
+    )
 }
